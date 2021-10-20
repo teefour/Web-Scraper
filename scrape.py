770466c7d2ea2f60a11ce9b17b414c537509341c
teefour <35282992+teefour@users.noreply.github.com>
2021-10-20 17:32:58 +1100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -20,8 +20,8 @@ for page in range(486, 1000):
         soup = bs4.BeautifulSoup(res.text, 'lxml')
         price = soup.select('h4')[0]
         item_title = soup.select('h4')[1]
-            prices.append(price.text)
-            titles.append(item_title.text)
+        prices.append(price.text)
+        titles.append(item_title.text)
         print(f'\n{item_title.text} costs {price.text}')
         page += 1
     else:
