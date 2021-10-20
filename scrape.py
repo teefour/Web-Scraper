f28888d4cf079c39dd71114c826eb60cca6b7eff
teefour <35282992+teefour@users.noreply.github.com>
2021-10-20 17:28:23 +1100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -12,26 +12,15 @@ prices = []
 page = 1
 
 for page in range(486, 1000):
-       
     scraping_url = main_url.format(page)
-
     res = requests.get(scraping_url)
-    
     if 'Not Found' not in res.text:
-        
         soup = bs4.BeautifulSoup(res.text, 'lxml')
-            
         price = soup.select('h4')[0]
-
         item_title = soup.select('h4')[1]
-
 		prices.append(price.text)
-
 		titles.append(item_title.text)
-
         print(f'\n{item_title.text} costs {price.text}')
-     
         page += 1
-
     else:
     	break
